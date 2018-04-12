ad63707e4a12e5605501a3c9ebffba52886ba8ed
lie771 <lie771@newschool.edu>
2018-04-11 22:41:14 -0400

    Created Java-Assignment

---
+++ java-assignment/main.js
@@ -0,0 +1,28 @@
+var myWeekend = 'i cried';
+
+72 - 6
+61 + 5
+60 + 6
+11 * 6
+33 + 33
+132 / 2
+
+var groupOne = ['Emil', 'Kristen', 'Emily'];
+var groupTwo = ['Evelin', 'Esra', 'Prithi'];
+var groupThree = ['Yaren', 'Yangchen'];
+var groupFour = ['Jahyun', 'Seowon', 'Soyeon'];
+var groupFive = ['Vincent', 'Vinny'];
+var groupSix = ['Khe', 'Shuting'];
+
+var allStudentsHere = true;
+
+
+var studentsMissing = ['mySoul'];
+
+console.log(studentsMissing.length);
+
+// for ( var i = 0; i < 10; i++ ) {
+//   console.log('The index is', i);
+// }
+
+
