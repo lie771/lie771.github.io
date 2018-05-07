00fdfd4ff239c86415cbe0f008338a12407a38c8
lie771 <lie771@newschool.edu>
2018-05-06 20:49:57 -0400

    Added Illustration

--- dream_vaca/main.js
+++ dream_vaca/main.js
@@ -21,7 +21,7 @@
 //     return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 
-$("#move").mouseenter(function () {
+$(".icecream").mouseenter(function () {
     $(this).animate({
         top: Math.random() * 300
     }, 100);
