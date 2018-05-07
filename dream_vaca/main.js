a32c6c9fe778dd9db10c6c2c4f84743ff94aa9ce
lie771 <lie771@newschool.edu>
2018-05-06 22:16:32 -0400

    Fixed Mouseover

--- dream_vaca/main.js
+++ dream_vaca/main.js
@@ -1,3 +1,53 @@
+// var images = [ "./img/a.png", 
+// 			"./img/b.png", 
+// 			"./img/c.png",
+// 			"./img/d.png",
+// 			"./img/e.png" ];
+// var i = 0;
+
+// setInterval(function() {
+//        if(images.length == i){
+//                 i = 0;
+//             }
+//             else {
+//             document.getElementsByClassName("icecream").src = images[i]; 
+//             i++;
+
+//         }
+//         },3000);
+
+// var cycle = 0;
+// var images = [ "./img/a.png", 
+// 			"./img/b.png", 
+// 			"./img/c.png",
+// 			"./img/d.png",
+// 			"./img/e.png" ];
+ 
+// setInterval(function() {
+// 	if (cycle > 0 && cycle < 5) {
+// 		$(".one").css({opacity:"0"});
+// 		$(".two").css({opacity:"1"});
+// 	} else if (cycle > 5 && cycle < 10) { 
+// 		$(".two").css({opacity:"0"});
+// 		$(".three").css({opacity:"1"});
+// 	}
+// }, 10000);
+
+
+$(document).ready(function(){
+	$(".icecream").mouseenter(function(e){
+		var windowMaxX = $(window).width() - $(this).width();
+		var windowMaxY = $(window).height() - $(this).height();   
+      $(this).css({ left:getRandomInt(0, windowMaxX),
+                   top:getRandomInt(0, windowMaxY) 
+      });
+	});
+	function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+});
+
 // $(document).ready(function(){
 //     $(".one").mouseover(function(){
 //         var x = Math.floor(Math.random()*300);
@@ -9,24 +59,36 @@
 // });
 
 // $(document).ready(function(){
-// 	$('.icecream').mouseover(function(e){
-//     var maxX = $(window).width() - $(this).width();
-//     var maxY = $(window).height() - $(this).height();    
+// 	$(".icecream").mouseenter(function(e){
+//     var windowX = $(window).width() - $(this).width();
+//     var windowY = $(window).height() - $(this).height();    
 //     $(this).css({
-//         'left':getRandomInt(0, maxX),
-//         'top':getRandomInt(0, maxY)
+//         'left':getRandomInt(0, windowX),
+//         'top':getRandomInt(0, windowY)
 //     });
 // });
 // function getRandomInt(min, max) {
 //     return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 
-$(".icecream").mouseenter(function () {
-    $(this).animate({
-        top: Math.random() * 300
-    }, 100);
-    $(this).animate({
-        left: Math.random() * 300
-    }, 100);
+//works but so slow
+// $(".icecream").mouseenter(function () {
+//     $(this).animate({
+//         top: Math.random() * 300
+//     }, 100);
+//     $(this).animate({
+//         left: Math.random() * 300
+//     }, 100);
 
-});
+// });
+
+//WORKS
+// $(document).ready(function(){
+// 	$(".icecream").mouseenter(function(e){
+// 		var windowX = $(window).width() - $(this).width();
+// 		var windowY = $(window).height() - $(this).height();   
+//       $(this).css({ left:(Math.random()*$(window).width()-20),
+//                    top:(Math.random()*$(window).height()-20),  
+//       });
+// 	});
+// });
